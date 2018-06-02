718d7b219589e81660c3a12bcc9498f313afd691
Tim Kilian <mail@timkilian.pl>
2018-06-03 01:08:55 +0200

    Add hot top urls

--- app.js
+++ app.js
@@ -22,18 +22,27 @@ app.get('/reddit/', function (req, res) {
 });
 
 app.get('/reddit/auth', function (req, res) {
-    res.send(reddit.auth());
+    reddit.auth().then(result => res.send(result));
 });
 
 app.get('/reddit/r/:sub' , function (req, res) {
     reddit.subreddit(req.params.sub).then(slides => {
+        slidesService.newPresentation(slides);
         res.send(slides);
     });
 });
 
-app.get('/reddit/random' , function (req, res) {
-    reddit.subreddit('random').then(slides => {
-        res.send(slidesService.newPresentation(slides));
+app.get('/reddit/r/:sub/top' , function (req, res) {
+    reddit.subreddit(req.params.sub + '/top').then(slides => {
+        slidesService.newPresentation(slides);
+        res.send(slides);
+    });
+});
+
+app.get('/reddit/r/:sub/hot' , function (req, res) {
+    reddit.subreddit(req.params.sub + '/hot').then(slides => {
+        slidesService.newPresentation(slides);
+        res.send(slides);
     });
 });
 
