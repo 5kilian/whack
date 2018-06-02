e9c71a922f00b8502fdf7e24fdf702c545e0596b
Tim Kilian <mail@timkilian.pl>
2018-06-02 15:36:36 +0200

    Get access token

--- reddit.js
+++ reddit.js
@@ -2,35 +2,24 @@
  * Reddit subpart
  */
 
-const https = require('https');
 const request = require('request');
 
 module.exports = {
     get: function () {
+         request.post({
+                 url: 'https://www.reddit.com/api/v1/access_token?grant_type=password&username=whackaoke&password=whackathon2k18',
+                 headers: {
+                     'User-Agent': 'Mozilla/5.0',
+                     'Authorization': 'Basic S3JkWnVnX2hfRkRkbnc6NkhNQjJLN18xRDhTX29pMXZQRUhrdTZSbkpn'
+                 }
+             },
+             function (error, response, body) {
+                console.log('body:', body);
+            }
+        );
 
-         /*
-        https.get('https://www.reddit.com/api/v1/me', (resp) => {
-            let data = '';
-
-           
-            console.log(resp.body);
-
-            resp.on('data', (chunk) => {
-                data += chunk;
-
-                console.log(chunk);
-            });
-        });
-        */
-        request('http://www.google.com', function (error, response, body) {
-            console.log('body:', body); 
-        });
-
-            
-        
 
         return 'Hello Reddit!';
-
     },
 };
 
